0d7368adee2bbebd7c74ae310073e36c30be3957
Karl Baldelomar <karlthegreat18@gmail.com>
2022-10-25 16:54:20 +0800

    form management

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import RouteHandler from "./components/route-handler";
 import "./scss/index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,57 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { useForm, SubmitHandler } from "react-hook-form";
+// import useStateAsync from "react-use-state-async";
+
+interface LoginFormInput {
+  email: string;
+  password: string;
+}
 
 export default function () {
+  // const [formDisabled, setFormDisabled] = useStateAsync(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInput>();
+  const onSubmit: SubmitHandler<LoginFormInput> = (data) => {
+    console.log(data);
+    console.log("SUBMITTED");
+  };
+
   return (
-    <Container>
+    <Container className="vh100">
       <Row>
-        <Col xs={6}>test</Col>
-        <Col xs={6}>
+        {/* <Col xs={12}></Col> */}
+        <Col
+          xs={8}
+          className="mx-auto vh100 d-flex flex-column justify-content-center"
+        >
           <div className="paper">
-            <div className="form-group">
-              <div className="input-group">
-                <label className="">Email</label>
-                <input className="form-input" />
-              </div>
-            </div>
+            <div className="title-login text-center mb-3">LOGIN</div>
+            <form className="form-login" onSubmit={handleSubmit(onSubmit)}>
+              <input
+                className="mb-3"
+                placeholder="Email"
+                type="email"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                })}
+              />
+              <input
+                className="mb-3"
+                placeholder="Password"
+                type="password"
+                {...register("password", { required: true })}
+              />
+              <input
+                className="btn-login"
+                disabled={!!Object.keys(errors).length}
+                type="submit"
+              />
+            </form>
           </div>
         </Col>
       </Row>
