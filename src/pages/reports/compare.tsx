73cde48541be8ab49f824a4ebbed9ddff176ef7c
karl baldelomar <kbaldelomar@vostronet.com>
2022-11-29 00:24:18 +0800

    compare difference

--- src/pages/reports/compare.tsx
+++ src/pages/reports/compare.tsx
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import ReactDiffViewer from "react-diff-viewer";
 import { getDocumentByIdAPI } from "../../components/api/index";
 import reportFields from "../../components/utils/report-fields";
 import Panel from "../../components/shared/panel";
@@ -7,53 +8,75 @@ import Loader from "../../components/shared/loader";
 import { faPrint, faMultiply, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { useParams, useLocation, Link } from "react-router-dom";
 import moment from "moment";
+import formFields from "../../components/utils/report-fields";
 
 export default () => {
   const { id, historyId } = useParams();
   const [document, setDocument] = useState();
   const [history, setHistory] = useState([]);
+
+  // fetching
   useEffect(() => {
     const fetchAPI = async () => {
       const result = await getDocumentByIdAPI(id);
       if (result?.status === 200) {
-        setDocument(result?.data?.result || []);
-        setHistory(result?.data?.history || []);
+        const changes = result?.data?.history || [];
+        const currentDocument = result?.data?.result || [];
+        setHistory(changes);
+        setDocument(currentDocument);
       }
     };
     fetchAPI();
   }, [setDocument]);
 
-  const renderFields = (document: object) => {
-    console.log({ document });
-    return reportFields
-      .filter(({ field }: { field: keyof object }) => !!document[field])
-      .map(
-        ({
-          field,
-          label,
-          type,
-        }: {
-          field: keyof object;
-          label: string;
-          type: string;
-        }) => {
-          return (
-            <Col xs={12} key={field}>
-              <div key={field} className="report-group mb-3">
-                <label>{label}</label>
-                {type === "text" ? (
-                  <div className="report-field">{document[field]}</div>
-                ) : (
-                  <div
-                    className="report-field"
-                    dangerouslySetInnerHTML={{ __html: document[field] }}
-                  />
-                )}
-              </div>
-            </Col>
-          );
-        }
-      );
+  if (!(document && history.length > 0)) {
+    return <Loader />;
+  }
+
+  const versionedDocument = history.reduce((prev, value) => {
+    return value?.id === Number(historyId) ? JSON.parse(value?.value) : prev;
+  }, {});
+
+  const compare = formFields.reduce((prev: object, value) => {
+    let current = document[value.field];
+    if (!!current) {
+      var div = window.document.createElement("div");
+      div.innerHTML = current;
+      current = div.textContent || div.innerText || "";
+    }
+
+    let versioned = versionedDocument[value.field];
+    if (!!versioned) {
+      var div = window.document.createElement("div");
+      div.innerHTML = versioned;
+      versioned = div.textContent || div.innerText || "";
+    }
+    prev[value.field] = {
+      current,
+      versioned,
+      label: value.label,
+    };
+    return prev;
+  }, {});
+
+  const renderDiff = () => {
+    return (Object.keys(compare) || [])
+      .filter(
+        (field) => !(!compare[field]?.current && !compare[field]?.versioned)
+      )
+      .map((field, index) => {
+        return (
+          <Row key={index}>
+            <div className="diff-label">{compare[field]?.label || ""}</div>
+            <ReactDiffViewer
+              showDiffOnly={false}
+              oldValue={compare[field]?.versioned || ""}
+              newValue={compare[field]?.current || ""}
+              splitView={true}
+            />
+          </Row>
+        );
+      });
   };
   return (
     <Container className="my-5">
@@ -73,7 +96,6 @@ export default () => {
                       updated_at: string;
                       id: string | number;
                     }) => {
-                      console.log({ _historyId, historyId });
                       return (
                         <Row className="row-alternate" key={_historyId}>
                           <Link
@@ -117,31 +139,26 @@ export default () => {
               },
             ]}
           >
-            <Row>
+            {/* <Row>
               <Col xs={6}>{document ? renderFields(document) : <Loader />}</Col>
               <Col xs={6} className="diff-bl">
-                {!document ? (
-                  <Loader />
-                ) : (
-                  renderFields(
-                    history.reduce((prev, value) => {
-                      // console.log({ valueId: value?.id, historyId, value });
-                      // if (value?.id === Number(historyId)) {
-                      //   const parsedValue = JSON.parse(value?.value);
-                      //   console.log({ parsedValue });
-                      //   return parsedValue;
-                      // }
-
-                      // return prev;
-
-                      return value?.id === Number(historyId)
-                        ? JSON.parse(value?.value)
-                        : prev;
-                    }, {})
-                  )
-                )}
+                {!document ? <Loader /> : renderFields(diffDocument)}
               </Col>
+            </Row> */}
+            <Row className="mb-3">
+              <Col>
+                {`Version: ${history.reduce(
+                  (prev, value) =>
+                    value.id == historyId
+                      ? moment(value.created_at).format("MMMM D YYYY h:mm:ss A")
+                      : prev,
+                  ""
+                )}`}
+              </Col>
+              <Col>Current Version</Col>
             </Row>
+
+            <Row>{renderDiff()}</Row>
           </Panel>
         </Col>
       </Row>
